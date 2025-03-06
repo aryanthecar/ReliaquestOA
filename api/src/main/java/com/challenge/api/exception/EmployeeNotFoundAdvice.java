fc9e66722ede4579bebd2e6865bf04a6e9780caa
aryanthecar <athakkar2135@gmail.com>
2025-03-06 10:30:28 -0500

    implemented exception handling to provide meaningful output when wrong request made

---
+++ api/src/main/java/com/challenge/api/exception/EmployeeNotFoundAdvice.java
@@ -0,0 +1,19 @@
+package com.challenge.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Advice for when employee with UUID not found, so that meaningful output is presented to user
+ */
+@RestControllerAdvice
+public class EmployeeNotFoundAdvice {
+
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
---
+++ api/src/main/java/com/challenge/api/exception/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.challenge.api.exception;
+
+import java.util.UUID;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(UUID uuid) {
+        super("Employee with UUID " + uuid + " not found");
+    }
+}
